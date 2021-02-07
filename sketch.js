a0f3a0e6295d91d2ad79839d4e2989227cb0f0c0
amairahjain <76904845+amairahjain@users.noreply.github.com>
2021-02-07 17:24:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,11 @@ function setup(){
 
     ground = Bodies.rectangle(200,390,200,20,ground_options);
     World.add(world,ground);
-
+    var ball_options ={
+        restitution : 1.0
+    }
+ball = Bodies.circle(200,100,20,ball_options)
+World.add(world,ball) 
     console.log(ground);
 }
 
@@ -25,4 +29,6 @@ function draw(){
     Engine.update(engine);
     rectMode(CENTER);
     rect(ground.position.x,ground.position.y,400,20);
+    ellipseMode(RADIUS)
+    ellipse(ball.position.x,ball.position.y,20,20)
 }
